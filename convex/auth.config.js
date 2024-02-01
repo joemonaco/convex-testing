d11a1936c3e973dee3bf2db2f47d3cb01136998c
Joe Monaco <joe@macbook-pro.mynetworksettings.com>
2024-02-01 14:47:43 -0500

    Initial convex and clerk setup

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://still-spider-67.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ convex/thumbnails.ts
@@ -0,0 +1,35 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createThumbnail = mutation({
+  args: {
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new Error("You must be logged in to create a new thumbnail");
+    }
+    await ctx.db.insert("thumbnails", {
+      title: args.title,
+      userId: user.subject,
+    });
+  },
+});
+
+export const getThumbnailsForUser = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      return [];
+      throw new Error("You must be logged in to create a new thumbnail");
+    }
+    return await ctx.db
+      .query("thumbnails")
+      .filter((q) => q.eq(q.field("userId"), user.subject))
+      .collect();
+  },
+});
